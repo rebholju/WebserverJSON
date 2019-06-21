8217c5cadddadcbcb6d0ad7a917ec781b8a67c61
rebholju <50166034+rebholju@users.noreply.github.com>
2019-06-21 17:43:45 +0200

    First Commit

---
+++ WebserverJSON/src/Main.java
@@ -0,0 +1,264 @@
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.rmi.Naming;
+
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+
+import javax.print.attribute.DateTimeSyntax;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+	 class VehicleDbModel{	
+		
+		 private Connection conn = null;
+	     private Statement statement = null;
+	     private PreparedStatement preparedStatement = null;
+	     private ResultSet resultSet = null;
+	     private int result=0;
+		 
+		 public VehicleDbModel()
+		 {
+			 //https://www.vogella.com/tutorials/MySQLJava/article.html
+			 	 
+			 try
+			 {
+			 Class.forName("com.mysql.jdbc.Driver");
+			 this.conn = DriverManager.getConnection("jdbc:mysql://localhost/SysArch","root", "");
+			 System.out.print("Database is connected !");
+
+//	            this.conn.close();
+			 }
+			 catch(Exception e)
+			 {
+			 System.out.print("Do not connect to DB - Error:"+e);
+			 }
+			 
+		 }
+		 
+		 
+			 
+		 
+		 
+		 
+		 
+		public void Main()
+		{	
+			//JSON parser object to parse read file
+			JSONParser jsonParser = new JSONParser();
+			
+			try (FileReader reader = new FileReader("WikiBeispiel.json"))
+			{
+				//Read JSON file
+	            Object obj = jsonParser.parse(reader);
+	            
+	            setVehicleData(obj,1);
+	            
+	        } catch (FileNotFoundException e) {
+	            e.printStackTrace();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        } catch (ParseException e) {
+	            e.printStackTrace();
+	        }
+			
+			
+			
+			try (FileReader reader = new FileReader("AuthRequest.json"))
+			{
+				//Read JSON file
+	            Object obj = jsonParser.parse(reader);
+	            
+	            JSONObject Response = authentificateDriver(obj);
+	            System.out.println(Response);
+	            
+	        } catch (FileNotFoundException e) {
+	            e.printStackTrace();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        } catch (ParseException e) {
+	            e.printStackTrace();
+	        }
+
+		}
+		
+		public boolean setVehicleData(Object obj, int vehicleNumber)
+		{
+			
+		    JSONObject data = (JSONObject) obj;
+		    
+		    JSONArray sensors = (JSONArray) data.get("Sensors");
+		    JSONObject passengers = (JSONObject) data.get("Passengers");
+		    
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss'Z'");
+			LocalDateTime parsedDatePassenger = LocalDateTime.parse(passengers.get("timestamp").toString(),formatter);
+			java.sql.Timestamp sqlTimestampPassenger = java.sql.Timestamp.valueOf(parsedDatePassenger);
+		
+		    for(int i= 0;i<sensors.size();i++)
+		    {
+		        JSONObject singleSensor = (JSONObject) sensors.get(i);
+		        
+//		        System.out.println(singleSensor.get("name"));
+//		        System.out.println(singleSensor.get("value"));
+//		        System.out.println(singleSensor.get("unit"));
+//		        System.out.println(singleSensor.get("state"));
+//		        System.out.println(singleSensor.get("timestamp"));
+//		        System.out.println(singleSensor.get("name").toString());
+		        
+//				java.sql.Date sqlDate = java.sql.Date.valueOf(parsedDate.toLocalDate());
+//				java.sql.Time sqlTime = java.sql.Date.valueOf(parsedDate.toLocalTime());
+//				System.out.println(sqlTimestamp);
+//				System.out.println(sqlDate);
+		        
+				LocalDateTime parsedDate = LocalDateTime.parse(singleSensor.get("timestamp").toString(),formatter);
+				java.sql.Timestamp sqlTimestamp = java.sql.Timestamp.valueOf(parsedDate);
+
+			    try {
+ 
+				    this.preparedStatement = this.conn.prepareStatement("UPDATE vehiclecurrentdata SET value=?, driver=?, timestamp=? WHERE vehicleNumber = ? AND sensor = ?");
+				    this.preparedStatement.setString(1,singleSensor.get("value").toString());
+				    this.preparedStatement.setString(2, passengers.get("name").toString());
+				    this.preparedStatement.setTimestamp(3, sqlTimestamp);
+				    this.preparedStatement.setInt(4, vehicleNumber);
+				    this.preparedStatement.setString(5, singleSensor.get("name").toString());		    
+		            result = this.preparedStatement.executeUpdate();
+//		            System.out.println(result);
+		            System.out.println("Wrote into vehiclecurrentdata");
+  
+			    }
+			    catch(Exception ex)
+			    {
+			    	System.out.println("SensorWert noch nicht vorhanden oder Datenbank nicht verbunden"+ex);
+			    }
+		    	
+			    
+		    	try {
+				    	if( result==0 )
+				    	{			    		
+						    this.preparedStatement = this.conn.prepareStatement("INSERT INTO vehiclecurrentdata(vehicleNumber, sensor, value, timeStamp, driver) values(?, ?, ?, ?, ?)");
+						    this.preparedStatement.setInt(1, vehicleNumber);
+						    this.preparedStatement.setString(2, singleSensor.get("name").toString());
+						    this.preparedStatement.setString(3, singleSensor.get("value").toString());
+						    this.preparedStatement.setTimestamp(4, sqlTimestamp);
+						    this.preparedStatement.setString(5, passengers.get("name").toString());
+						    result = this.preparedStatement.executeUpdate();
+//						    System.out.println(resultSet);
+						    System.out.println("Wrote into vehiclecurrentdata");
+				    	}
+		    		}
+			    	catch(Exception exception)
+			    	{
+			    		System.out.println("Fehler 2te "+exception);
+			    	}
+				    
+		    	try {
+		    		
+					    this.preparedStatement = this.conn.prepareStatement("INSERT INTO vehiclehistoricaldata(vehicleNumber, sensor, value, timeStamp, driver) values(?, ?, ?, ?, ?)");
+					    this.preparedStatement.setInt(1, vehicleNumber);
+					    this.preparedStatement.setString(2, singleSensor.get("name").toString());
+					    this.preparedStatement.setString(3, singleSensor.get("value").toString());
+					    this.preparedStatement.setTimestamp(4, sqlTimestamp);
+					    this.preparedStatement.setString(5, passengers.get("name").toString());
+					    result = this.preparedStatement.executeUpdate();
+//					    System.out.println(resultSet);
+					    System.out.println("Wrote into vehiclehistoricaldata");
+			    	
+	    		}
+		    	catch(Exception exception)
+		    	{
+		    		System.out.println("Fehler 2te "+exception);
+		    	}
+			     
+		      
+		    }
+
+		    return true;
+		}
+
+		//authentificate Driver
+		public JSONObject authentificateDriver(Object obj)
+		{
+      	  String firstname = null;
+      	  String lastname = null;
+      	  String role = null;
+			
+			
+			JSONObject data = (JSONObject) obj;
+			int rfidID = Integer.parseInt(data.get("id").toString());
+			//System.out.println(rfidID);
+			
+	            try {
+	            	
+		            this.preparedStatement = this.conn.prepareStatement("select firstname, lastname, role from users where rfidID=?");
+		            this.preparedStatement.setInt(1,rfidID);
+		            this.resultSet = this.preparedStatement.executeQuery();
+		            
+		            while (resultSet.next()) {
+		            	int i = 0;
+		            	  firstname = resultSet.getString("firstname");
+		            	  lastname = resultSet.getString("lastname");
+		            	  role = resultSet.getString("role");
+		            	  if(i<0)
+		            	  {
+		            		  System.out.println("RFID mehrmals vergeben");
+		            	  }
+		            	  i++;
+		            	}
+		            
+	            }
+	            catch(Exception ex)
+	            {
+	            	System.out.println("Fehler mit Datenbank");
+	            }
+	            
+
+			
+			if(firstname!=null && lastname!=null && role!=null)
+			{
+			JSONObject AuthResponse = new JSONObject();
+			
+			AuthResponse.put( "id" , rfidID);                    
+			AuthResponse.put( "firstName"  , firstname);           
+			AuthResponse.put("lastName"  , lastname);   
+			AuthResponse.put( "authLevel"  , role);
+			return AuthResponse;
+			}
+			else
+			{
+				return null;
+			}
+				
+		}
+
+
+	}
+
+		
+		
+
+		
+		public class Main {
+		
+			public static void main(String[] args) 
+			{
+
+				VehicleDbModel refVehicleDbModel = new VehicleDbModel();
+				refVehicleDbModel.Main();
+			}
+		}
+
+
+
+
+
